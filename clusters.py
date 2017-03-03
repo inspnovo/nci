6dd650623f17e2bd33b57df37ecaa9ce5c88f65c
guyihui <inspnovo@163.com>
2017-03-03 13:39:12 +0800

    end of nn

--- clusters.py
+++ clusters.py
@@ -107,7 +107,8 @@ import random
 def kcluster(rows,distance=pearson,k=4):
     ranges=[(min([row[i] for row in rows]),max([row[i] for row in rows])) for i in range(len(rows[0]))]
 
-    clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0] for i in range(len(rows[0]))] for j in range(k)]
+    clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0] for i in range(len(rows[0]))] 
+                for j in range(k)]
 
     lastmatches=None
     for t in range(100):
